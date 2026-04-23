f1f5045715fa9c5415c5552662a9700a8b0aec28
Kofiro <kofibeduaustin222@gmail.com>
2026-04-23 22:18:30 +0000

    added lookat which takes transposed rotation and inversed translation, since view moves world opposite of camera

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,30 @@ const unsigned int HEIGHT { 600 };
 
 float mixVal = 0.2f;
 
+float deltaTime = 0.0f; // time between current frame and last frame
+float lastFrame = 0.0f; // time of last frame
+
+
+// camera pos
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+// camera direction
+glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+// cam right
+glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraRight = glm::normalize(glm::cross(worldUp, cameraDirection));
+// cam up
+glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+// camera front
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 void processMix(GLFWwindow* window, float *mixValue);
+
 
 
 int main() {
@@ -119,6 +139,7 @@ int main() {
         2, 3, 0
         
     };
+
 
     
     
@@ -215,18 +236,39 @@ int main() {
     //glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
     ourShader.setInt("texture2", 1);
 
-    
+    glEnable(GL_DEPTH_TEST);
 
-   
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.0f, -2.2f, -2.5f), 
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(-2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f),
+        glm::vec3(3.0f, 4.0f, 4.5f),
+        glm::vec3(5.0f, -5.0f, 10.0f),
+        glm::vec3(-4.0f, -0.7f, 3.9f),
+        glm::vec3(2.5f, -1.3f, 6.5f),
+        glm::vec3(-6.0f, 1.5f, 3.0f)
+       
+    };
+
     // render loop
     while(!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
          // input
         processInput(window);
 
 
 
         glClearColor(0.2f, 0.3f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -239,31 +281,44 @@ int main() {
         
         ourShader.setFloat("mixVal", mixVal);
 
+        
+
         // translate +  rotation over time
         // glm::mat4 trans = glm::mat4(1.0f);
         // trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 0.0f));
         // trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
         // modelmatrix
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-    // view matrix
-    glm::mat4 view = glm::mat4(1.0f);
-    // translate scene in reverse direction of where we want to move
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        // glm::mat4 model = glm::mat4(1.0f);
+        // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-    // projection matrix
-    glm::mat4 projection;
-    projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+
+        // view matrix
+        glm::mat4 view = glm::mat4(1.0f);
+        // translate scene in reverse direction of where we want to move
+        //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, worldUp);
+
+        // projection matrix
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(60.0f), 800.0f/600.0f, 0.1f, 100.0f);
         
-        ourShader.setMatrix4("model", model);
+       // ourShader.setMatrix4("model", model);
         ourShader.setMatrix4("view", view);
         ourShader.setMatrix4("projection", projection);
 
         // ourShader.setMatrix4("transform", trans);
         
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0, 36);
+        for(unsigned int i = 0; i < 15; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMatrix4("model", model);
+            glDrawArrays(GL_TRIANGLES,0, 36);
+        }
+       
         //glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
@@ -290,7 +345,16 @@ void processInput(GLFWwindow* window) {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-        processMix(window, &mixVal);
+    //processMix(window, &mixVal);
+    const float cameraSpeed = 2.5f * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 void processMix(GLFWwindow* window, float *mixvalue) {
